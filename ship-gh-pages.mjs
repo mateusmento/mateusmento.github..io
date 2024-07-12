4fd0c49c84b94c2aa15dfbdca7851ee28969bc4c
Mateus Sarmento <omateusmento@gmail.com>
2024-07-12 19:44:30 -0300

    Update release script

--- ship-gh-pages.mjs
+++ ship-gh-pages.mjs
@@ -8,12 +8,12 @@ import * as fs from "fs";
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("copy", ["src/assets", folderName]);
+    await execa("cp", ["-r", "src/assets", folderName]);
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("del", ["-r", folderName]);
+    await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
